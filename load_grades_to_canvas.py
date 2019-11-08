3cf210e2d5f4a095cd38b6ccbc145c5335f809e7
heikalb <heikal93@gmail.com>
2019-11-07 16:10:07 -0800

    Added useful feature

--- load_grades_to_canvas.py
+++ load_grades_to_canvas.py
@@ -1,21 +1,55 @@
 """
-Add scores from scantron test results to a Canvas gradebook
+Load scores from scantron test results to a Canvas gradebook.
+Heikal Badrulhisham, 2018 <heikal93@gmail.com>
 """
-
+import argparse
 import csv
 
 
+def load_parameters(file_path):
+    """
+    Get parameters from parameter file
+    :param file_path:
+    :return: dictionary of parameters
+    """
+    # Read parameter file
+    with open(file_path, 'r') as f:
+        file_lines = f.read().split('\n')
+        input_lines = [l for l in file_lines if l and l[0] != '#']
+
+    # Build parameter dictionary
+    parameter_names = [l.split(':')[0].strip() for l in input_lines]
+    parameter_values = [l.split(':')[1].strip() for l in input_lines]
+    parameters = dict(zip(parameter_names, parameter_values))
+
+    # Type cast numerical parameters
+    parameters['source_id_index'] = int(parameters['source_id_index'])
+    parameters['target_id_index'] = int(parameters['target_id_index'])
+    parameters['source_index'] = int(parameters['source_index'])
+    parameters['target_index'] = int(parameters['target_index'])
+
+    return parameters
+
+
 def main():
+    # Get user specified parameter file
+    parser = argparse.ArgumentParser(description='Load scores from scantron test results to a Canvas gradebook.')
+    parser.add_argument('-params', default='parameters.txt', help='.txt file containing task parameters')
+    args = parser.parse_args()
+
+    # Inputted parameters for current grade loading
+    parameters = load_parameters(args.params)
+
     # Data file names
-    source_file_path = 'canvas.csv'
-    target_file_path = '2019-06-27T1809_Grades-LING111_D100.csv'
-    save_file_path = '2019-06-27T1809_Grades-LING111_D100_.csv'
+    source_file_path = parameters['source_file_path']
+    target_file_path = parameters['target_file_path']
+    save_file_path = parameters['save_file_path']
 
     # Positions of variables of interest in data files
-    source_index = 5
-    target_index = 6
-    source_id_index = 2
-    target_id_index = 2
+    source_id_index = parameters['source_id_index']
+    target_id_index = parameters['target_id_index']
+    source_index = parameters['source_index']
+    target_index = parameters['target_index']
 
     # Source file reader
     source_file_reader = csv.reader(open(source_file_path, 'r'))
